d98bcadfc924dfd04cfcd9c614565f9123cb5065
lakhanyawa <lakhanyawa@gmail.com>
2020-03-10 05:19:30 -0400

    learning git repository

--- repocreate-remote.sh
+++ repocreate-remote.sh
@@ -3,7 +3,7 @@
 # take repo name from command line
 repo_name=$1
 test -z $repo_name && echo "Repo name required." 1>&2 && exit 1
-SITE_REPO_URL=https://github.com/lakhanyawa/$repo_name
+SITE_REPO_URL=https://github.com/$username/$repo_name
 git ls-remote "$SITE_REPO_URL" > /dev/null 2>&1
 if [ "$?" -ne 0 ]; then
     echo "[ERROR] Unable to read from '$SITE_REPO_URL'"
@@ -12,14 +12,14 @@ if [ "$?" -ne 0 ]; then
     old_stty_admin=$(stty -g)
     stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_admin # Careful playing with stty
     if echo "$answer" | grep -iq "^y" ;then
-       curl -u 'lakhanyawa:lakhan@lakhan@157' https://api.github.com/user/repos -d "{\"name\":\"$repo_name"admin"\"}"
+       curl -u '$username:$userpassword' https://api.github.com/user/repos -d "{\"name\":\"$repo_name"admin"\"}"
        mkdir $repo_name"admin"
        cd $repo_name"admin"
        echo test > README.md
        git init
        git add .
        git commit -m "learning git repository"
-       git remote add origin https://github.com/lakhanyawa/$repo_name"admin"
+       git remote add origin https://github.com/$username/$repo_name"admin"
        git push -u origin master
        git checkout -b dev
        git commit -a -m "created dev branch"
@@ -31,14 +31,14 @@ if [ "$?" -ne 0 ]; then
     old_stty_osadmin=$(stty -g)
     stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_osadmin # Careful playing with stty
     if echo "$answer" | grep -iq "^y" ;then
-       curl -u 'lakhanyawa:lakhan@lakhan@157' https://api.github.com/user/repos -d "{\"name\":\"$repo_name"osadmin"\"}"
+       curl -u '$username:$userpassword' https://api.github.com/user/repos -d "{\"name\":\"$repo_name"osadmin"\"}"
        mkdir $repo_name"osadmin"
        cd $repo_name"osadmin"
        echo test > README.md
        git init
        git add .
        git commit -m "learning git repository"
-       git remote add origin https://github.com/lakhanyawa/$repo_name"osadmin"
+       git remote add origin https://github.com/$username/$repo_name"osadmin"
        git push -u origin master
        git checkout -b dev
        git commit -a -m "created dev branch"
@@ -50,14 +50,14 @@ if [ "$?" -ne 0 ]; then
     old_stty_devops=$(stty -g)
     stty raw -echo ; answer=$(head -c 1) ; stty $old_stty_devops # Careful playing with stty
     if echo "$answer" | grep -iq "^y" ;then
-       curl -u 'lakhanyawa:lakhan@lakhan@157' https://api.github.com/user/repos -d "{\"name\":\"$repo_name"devops"\"}"
+       curl -u '$username:$userpassword' https://api.github.com/user/repos -d "{\"name\":\"$repo_name"devops"\"}"
        mkdir $repo_name"devops"
        cd $repo_name"devops"
        echo test > README.md
        git init
        git add .
        git commit -m "learning git repository"
-       git remote add origin https://github.com/lakhanyawa/$repo_name"devops"
+       git remote add origin https://github.com/$username/$repo_name"devops"
        git push -u origin master
        git checkout -b dev
        git commit -a -m "created dev branch"
@@ -69,4 +69,4 @@ else
         echo "Repository already present"
 fi
 
-#repo-check=`git ls-remote https://github.com/lakhanyawa/test`
+#repo-check=`git ls-remote https://github.com/$username/test`
